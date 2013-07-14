2896166c2076e2a75aa6644d5f725354ff595db6
csinva <csinva@yahoo.com>
2013-07-14 16:19:51 -0400

    Angular added; Input with dynamic update; Everything in index.html

--- test.js
+++
@@ -1,7 +0,0 @@
-
-var x = $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {page:"Football", prop:"text"}, function(data) {useData(data)});
-
-function useData(data){
-	console.log(data.parse.text);
-	console.log(data.parse.text['*']);
-}
