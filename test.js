c9c21e7e7a0e9ce4bdb634b0bbf5e315bae5fa35
csinva <csinva@yahoo.com>
2013-07-14 15:33:14 -0400

    Printed wiki-data as a string

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 
-var x = $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {page:"Football", prop:"text"}, function(data) {console.log(data);});console.log(x);
-console.log(x);
+var x = $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {page:"Football", prop:"text"}, function(data) {useData(data)});
+
+function useData(data){
+	console.log(data.parse.text);
+	console.log(data.parse.text['*']);
+}
